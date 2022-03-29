9dbed24b92308b68a699d299242412fce4708f80
A-d-f <annamaria.nappila@gmail.com>
2022-03-29 15:24:47 +0300

    Questions from database to a candidate view
    I made a jsp-file (based on Jennas html one) and made a servlet for bringing questions from a database to a .jsp-servlet. Update Dao-file by making a new method, which reads data from database to on arraylist. Data-file Questions.java for to a transfer data infos. Finally add this new servlet to an index-page.

---
+++ TheElectionMachine/src/main/java/app/AnswerQuestionsCandidate.java
@@ -0,0 +1,51 @@
+package app;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.Dao;
+import data.Questions;
+
+/**
+ * Servlet implementation class AnswerQuestionsCandidate
+ */
+@WebServlet("/AnswerQuestionsCandidate")
+public class AnswerQuestionsCandidate extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Dao dao = null;
+
+	public void init() {
+		dao = new Dao("jdbc:mysql://localhost:3306/minion", "admin", "password");
+	}
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AnswerQuestionsCandidate() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<Questions> list = null;
+		if (dao.getConnection()) {
+			list = dao.readAllQuestions();
+		} else {
+			System.out.println("Connection error");
+		}
+		request.setAttribute("questions", list);
+		RequestDispatcher rd = request.getRequestDispatcher("/jsp/answerquestioncand.jsp");
+		rd.forward(request, response);
+	}
+}
